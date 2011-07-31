498bce9964cd994703e33b0675a4e46bfd6656a5
Juanjo Bazán <jjbazan@gmail.com>
2011-07-31 20:13:31 +0200

    Errors

--- lib/nimbus/exceptions.rb
+++ lib/nimbus/exceptions.rb
@@ -1,4 +1,10 @@
 module Nimbus
-  
-  
+  class Error < StandardError; end
+  class InvalidOptionError < Error; end
+  class InputFileError < Error; end
+  class WrongFormattedFileError < Error; end
+  class ConfigurationError < Error; end
+  class ForestError < Error; end
+  class TreeError < Error; end
+  class IndividualError < Error; end
 end
